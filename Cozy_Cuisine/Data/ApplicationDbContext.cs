c53a5ec4945f41da9b18d1915d8b652fe884c150
RenzCodeRepo <renzbaladjay25@gmail.com>
2025-03-10 14:51:57 +0800

    - added new packages (downgrade to work on .net8) -- db context not yet finish -- with sqlserver

---
+++ Cozy_Cuisine/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿
+using Cozy_Cuisine.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace Cozy_Cuisine.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        public DbSet<About> About { get; set; }
+    }
+}
