08f8d0d8b8d02c50998397190aa27165e009dd45
HartFinne <167497706+HartFinne@users.noreply.github.com>
2025-03-10 15:04:50 +0800

    added models

--- Cozy_Cuisine/Data/ApplicationDbContext.cs
+++ Cozy_Cuisine/Data/ApplicationDbContext.cs
@@ -13,5 +13,21 @@ namespace Cozy_Cuisine.Data
 
         }
         public DbSet<About> About { get; set; }
+        public DbSet<BugReport> BugReport { get; set; }
+        public DbSet<Characters> Characters { get; set; }
+        public DbSet<Comments> Comments { get; set; }
+        public DbSet<Contacts> Contacts { get; set; }
+        public DbSet<Credit> Credit { get; set; }
+        public DbSet<FAQ> FAQ { get; set; }
+        public DbSet<Gallery> Gallery { get; set; }
+        public DbSet<GameDownloads> GameDownloads { get; set; }
+        public DbSet<GameItems> GameItems { get; set; }
+        public DbSet<GameMechanics> GameMechanics { get; set; }
+        public DbSet<GameReview> GameReview { get; set; }
+        public DbSet<Locations> Locations { get; set; }
+        public DbSet<Notice> Notice { get; set; }
+        public DbSet<Patches> Patches { get; set; }
+        public DbSet<StoryPlot> StoryPlot { get; set; }
+        public DbSet<Wiki> Wiki { get; set; }
     }
 }
